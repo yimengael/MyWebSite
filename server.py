6df71a1e02d05d307dda41ef9bd3e5c746c9fde8
Gael Yimen Yimga <yimengael@gmail.com>
2016-12-08 18:26:45 -0500

    Adding some form like resume, register, dashboard, aboutme

--- server.py
+++ server.py
@@ -19,6 +19,36 @@ def showContact():
     return render_template("contactUs.html")
 
 
+@app.route('/aboutme')
+def showAboutMe():
+    return render_template("aboutMe.html")
+
+
+@app.route('/registerCustomer')
+def showRegisterCustomer():
+    return render_template("registerCustomer.html")
+
+
+@app.route('/registerProject')
+def showRegisterProject():
+    return render_template("registerProject.html")
+
+
+@app.route('/dashboardCustomer')
+def showDashboardCustomer():
+    return render_template("dashboardCustomer.html")
+
+
+@app.route('/dashboardCustProject')
+def showDashboardProject():
+    return render_template("dashboardProject.html")
+
+
+@app.route('/resume')
+def showResume():
+    return render_template("resume.html")
+
+
 
 # Run the application
 if __name__ == '__main__':
